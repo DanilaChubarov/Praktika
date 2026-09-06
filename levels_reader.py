538fba140d35cf50735321e35dc29b96b123b8d9
DanilaChubarov <thubarov21@gmail.com>
2026-09-06 15:31:25 +0300

    a

--- levels_reader.py
+++ levels_reader.py
@@ -172,11 +172,9 @@ class LevelReader:
                 return port
         for port in self.sl_ports:
             if player_rect.colliderect(port.rect):
-                print("slow")
                 return port
         for port in self.sp_ports:
             if player_rect.colliderect(port.rect):
-                print("fast")
                 return port
               
         # 3. ПРИЗЕМЛЕНИЕ НА ПЛАТФОРМЫ И ПОЛУБЛОКИ
